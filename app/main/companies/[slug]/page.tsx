dd02b8baa683f97dfbd1cc80e96c74db3356c2db
Francesco Tovo <francesco.tovo96@gmail.com>
2025-03-22 21:38:24 +0100

    Migliorata gestione stato loading e feedback schermata palestra

--- app/main/companies/[slug]/page.tsx
+++ app/main/companies/[slug]/page.tsx
@@ -2,10 +2,10 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Card, CardContent, IconButton, Rating, Tooltip, Typography } from '@mui/material';
+import { Card, CardContent, IconButton, Rating, Skeleton, Snackbar, Tooltip, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { Activity } from '@/app/models/activity';
-import { LoginProvider } from '@/app/providers/userProvider';
+import { UserContext } from '@/app/providers/userProvider';
 import { Feedback } from '@/app/models/feedback';
 import { AccountCircleOutlined, ArrowBack } from '@mui/icons-material';
 import { useRouter } from 'next/navigation';
@@ -16,12 +16,18 @@ import { useParams } from 'next/navigation'
 import { Company } from '@/app/models/company';
 
 interface FeedbackCardProps {
-  feedback: Feedback,
+  feedback: Feedback | null,
 }
 
 function FeedbackCard(props: FeedbackCardProps) {
-  const userName = props.feedback.userName || 'Anonimo';
-  const date = new Date(props.feedback.timestamp).toLocaleDateString();
+  let feedbackFooter;
+  if (props.feedback) {
+    const userName = props.feedback.userName || 'Anonimo';
+    const date = new Date(props.feedback.timestamp).toLocaleDateString();
+    feedbackFooter = <>{userName} - {date}</>;
+  } else {
+    feedbackFooter = <Skeleton width={96} />;
+  }
   return (
     <Card sx={{
       minWidth: 275, flexGrow: 1,
@@ -38,13 +44,17 @@ function FeedbackCard(props: FeedbackCardProps) {
         flexGrow: 1
       }}>
         <Box>
-          <Rating name="read-only" value={props.feedback.score} readOnly />
-          <Typography gutterBottom sx={{ fontSize: 20, fontWeight: 'bold' }}>{props.feedback.message}</Typography>
+          {
+            props.feedback ?
+              <Rating name="read-only" value={props.feedback.score} readOnly /> :
+              <Skeleton width={64} />
+          }
+          <Typography gutterBottom sx={{ fontSize: 20, fontWeight: 'bold' }}>{props.feedback?.message || <Skeleton />}</Typography>
           <Box sx={{ paddingBottom: 1 }} />
         </Box>
         <Typography sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'start', alignItems: 'center', gap: 1, paddingBottom: 1 }}>
           <AccountCircleOutlined />
-          {userName} - {date}
+          {feedbackFooter}
         </Typography>
       </CardContent>
     </Card>
@@ -52,7 +62,8 @@ function FeedbackCard(props: FeedbackCardProps) {
 }
 
 export default function ActivitiesPage() {
-  const [loading, setLoading] = React.useState(false);
+  const [isLoadingFeedbacks, setIsLoadingFeedbacks] = React.useState(false);
+  const [isLoadingActivities, setIsLoadingActivities] = React.useState(false);
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
   const [activities, setActivities] = React.useState<Activity[]>([]);
   const [feedbacks, setFeedbacks] = React.useState<Feedback[]>([]);
@@ -61,19 +72,16 @@ export default function ActivitiesPage() {
   const router = useRouter();
   const params = useParams<{ slug: string; }>();
   const [company, setCompany] = React.useState<Company | null>(null);
- 
-  // useEffect(() => {
-  //   _loadFeedbacks();
-  //   _loadActivities();
-  // }, []);
+  const [snackbarMessage, setSnackbarMessage] = React.useState<string>('');
+  const user = React.useContext(UserContext);
+
   useEffect(() => {
     _loadCompany();
-  }, [params]);
+  }, [params, user.isLoggedIn]);
 
   async function _loadActivities() {
-    setLoading(true);
+    setIsLoadingActivities(true);
     const queryParams: Map<string, string | null> = new Map<string, string | null>();
-    const user = LoginProvider.getUser();
     if (user.isLoggedIn) {
       queryParams.set('userId', `${user.id}`);
     }
@@ -82,7 +90,7 @@ export default function ActivitiesPage() {
     setActivities(_activities);
     console.log(activities);
     timeoutRef.current = null;
-    setLoading(false);
+    setIsLoadingActivities(false);
   }
 
   async function _loadCompany() {
@@ -93,8 +101,10 @@ export default function ActivitiesPage() {
   }
 
   async function _loadFeedbacks() {
+    setIsLoadingFeedbacks(true);
     const _feedbacks = await loadFeedbacks(`${params.slug}`);
     setFeedbacks(_feedbacks);
+    setIsLoadingFeedbacks(false);
   }
 
   function handleClickActivity(activity: Activity) {
@@ -117,14 +127,17 @@ export default function ActivitiesPage() {
     console.log(params);
     const reservation = await reserveActivity(params);
     if (reservation === null) {
-      console.error('error!');
+      setSnackbarMessage('Impossibile prenotare l\'attività');
     } else {
+      setSnackbarMessage('Attività prenotata');
       selectedActivity.current = null;
       setIsDialogOpen(false);
       _loadActivities();
     }
   }
 
+  const feedbacksToRender = isLoadingFeedbacks ? [null, null] : feedbacks;
+  const activitiesToRender = isLoadingActivities ? [null, null, null, null] : activities;
   return (
     <>
       <Box sx={{ padding: 2, }}>
@@ -136,22 +149,28 @@ export default function ActivitiesPage() {
               </IconButton>
             </Tooltip>
           </Box>
-          <Typography sx={{ fontSize: 26, fontWeight: 'bold' }}>{company?.name || ''}</Typography>
+          <Typography sx={{ fontSize: 26, fontWeight: 'bold' }}>{company?.name || <Skeleton width={120} />}</Typography>
         </Box>
         <Typography sx={{ fontSize: 20, fontWeight: 'bold', paddingBottom: 2 }}>Gli ultimi feedback</Typography>
         <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', rowGap: 2, columnGap: 2 }}>
-          {feedbacks.map(feedback =>
-            <FeedbackCard key={feedback.id} feedback={feedback} />
+          {feedbacksToRender.map((feedback, index) =>
+            <FeedbackCard key={feedback?.id || index} feedback={feedback} />
           )}
         </Box>
         <Typography sx={{ fontSize: 20, fontWeight: 'bold', paddingTop: 6, paddingBottom: 2 }}>Attività</Typography>
         <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', rowGap: 2, columnGap: 2 }}>
-          {activities.map(activity =>
-            <ActivityCard key={activity.id} activity={activity} handleClickActivity={handleClickActivity} isLoggedIn={LoginProvider.isLoggedIn()} showCompanyLink={false} />
+          {activitiesToRender.map((activity, index) =>
+            <ActivityCard key={activity?.id || index} activity={activity} handleClickActivity={handleClickActivity} isLoggedIn={user.isLoggedIn} showCompanyLink={false} />
           )}
         </Box>
       </Box>
       {isDialogOpen ? <ActivityDialog isOpen={isDialogOpen} activity={selectedActivity.current!} handleClose={handleCloseDialog} handleReserveActivity={() => handleReserveActivity()}></ActivityDialog> : null}
+      <Snackbar
+        open={!!snackbarMessage}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarMessage('')}
+        message={snackbarMessage}
+      />
     </>
   );
 }
